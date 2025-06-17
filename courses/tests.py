5939584bb5fe6413ba4f4879f3dbf1f3d394fa7d
Pavel Polezhaev <pavelpoleczaev@gmail.com>
2025-06-17 22:07:15 +0300

    add workflows

--- courses/tests.py
+++ courses/tests.py
@@ -1,4 +1,4 @@
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from django.test import TransactionTestCase

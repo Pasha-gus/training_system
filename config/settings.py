5520f0e0a6a4a491a86eb562b65473a5f5cc9ece
Pavel Polezhaev <pavelpoleczaev@gmail.com>
2025-06-17 22:05:49 +0300

    add workflows

--- config/settings.py
+++ config/settings.py
@@ -173,8 +173,13 @@ if 'test' in sys.argv:
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "test_db.sqlite3",
+            "TEST": {
+                "NAME": BASE_DIR / "test_db.sqlite3",
+            },
+            "ATOMIC_REQUESTS": False
         }
     }
+
 else:
     DATABASES = {
         "default": {
